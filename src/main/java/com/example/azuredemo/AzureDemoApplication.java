ea23214532130229015d0a3bda65dbe39ceccfa7
Nima <saminasab.nima@gmail.com>
2023-01-13 09:48:11 +0100

    Første utkast

---
+++ src/main/java/com/example/azuredemo/AzureDemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.azuredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AzureDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureDemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String helloWorld(@RequestParam(defaultValue = "Cloud") String name) {
+		return "Hello " + name;
+	}
+
+}
